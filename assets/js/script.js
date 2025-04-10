dc8037857a648838ec8beee9276863456ca72878
braia <braianfreitasdelima@gmail.com>
2025-04-10 17:38:43 -0300

    redirect de orçamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
 function handleSubmit(event) {
-  event.preventDefault();
-  
-  const formData = new FormData(event.target);
-  const data = Object.fromEntries(formData.entries());
-  
-  // Aqui você pode adicionar a lógica para enviar os dados do formulário
-  console.log('Dados do formulário:', data);
-  
-  // Exemplo de feedback para o usuário
-  alert('Obrigado! Entraremos em contato em breve.');
-  event.target.reset();
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+
+    const textoWhatsapp =
+        'Olá, me chamo ' + data.name + '.' +
+        '\nGostaria de solicitar um orçamento, segue minhas informações e projeto.' +
+        '\nTelefone: ' + data.phone +
+        '\nE-mail: ' + data.email +
+        '\nProjeto: *' + data.project + '*';
+
+    let textoFinal = textoWhatsapp;
+    if ((data.message ?? '').length > 0) {
+        textoFinal += '\nNecessidades e ideias: ' + data.message;
+    }
+
+    const numeroDestino = '5541999333572'; // Substitua pelo número do WhatsApp desejado (com DDI + DDD)
+    const urlWhatsapp = `https://wa.me/${numeroDestino}?text=${encodeURIComponent(textoFinal)}`;
+
+    const link = document.createElement('a');
+    link.href = urlWhatsapp;
+    link.target = '_blank';
+    link.click();
 }
